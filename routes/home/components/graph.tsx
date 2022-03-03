0d8fddc1678567dfe588caf3bbe9f1b2a814db29
Bereket Kibru <bekakibru2@gmail.com>
2022-03-03 10:56:15 +0300

    skill graph with dumy static data

--- routes/home/components/graph.tsx
+++ routes/home/components/graph.tsx
@@ -31,17 +31,21 @@ export default function Graph({ data }) {
                   nodeAutoColorBy="selected"
                   width={500}
                   height={500}
-                  minZoom={2  }
+                  minZoom={2.5}
                   nodeCanvasObject={(node, ctx, globalScale) => {
                     const label = node.id;
-                    const fontSize = 12 / globalScale;
+                    const fontSize = 15 / globalScale;
                     ctx.font = `${fontSize}px Sans-Serif`;
                     const textWidth = ctx.measureText(label).width;
                     const bckgDimensions = [textWidth, fontSize].map(
                       (n) => n + fontSize * 0.2
                     ); // some padding
-                    ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
-            ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+                    ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+                    ctx.fillRect(
+                      node.x - bckgDimensions[0] / 2,
+                      node.y - bckgDimensions[1] / 2,
+                      ...bckgDimensions
+                    );
                     ctx.textAlign = "center";
                     ctx.textBaseline = "middle";
                     ctx.fillStyle = node.color;
@@ -52,7 +56,12 @@ export default function Graph({ data }) {
                   nodePointerAreaPaint={(node, color, ctx) => {
                     ctx.fillStyle = color;
                     const bckgDimensions = node.__bckgDimensions;
-                    bckgDimensions && ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+                    bckgDimensions &&
+                      ctx.fillRect(
+                        node.x - bckgDimensions[0] / 2,
+                        node.y - bckgDimensions[1] / 2,
+                        ...bckgDimensions
+                      );
                   }}
                 />
               </Box>
