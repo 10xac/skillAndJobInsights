185bdb6bfe87b187988de68b151f9b3964c17730
Bereket Kibru <bekakibru2@gmail.com>
2022-03-08 20:09:14 +0300

    related skills

---
+++ backend/src/api/related-skill/controllers/related-skill.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  related-skill controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::related-skill.related-skill');
---
+++ backend/src/api/related-skill/routes/related-skill.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * related-skill router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::related-skill.related-skill');
